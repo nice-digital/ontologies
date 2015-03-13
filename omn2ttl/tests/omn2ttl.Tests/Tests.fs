6d5b879eea24485f8e853b1f36c6e5eca520032a
Nathan Smith <nathan@smeef.co.uk>
2015-03-13 14:25:51 +0000

    initial commit of omn2ttl

---
+++ omn2ttl/tests/omn2ttl.Tests/Tests.fs
@@ -0,0 +1,10 @@
+module omn2ttl.Tests
+
+open omn2ttl
+open NUnit.Framework
+
+[<Test>]
+let ``hello returns 42`` () =
+  let result = Library.hello 42
+  printfn "%i" result
+  Assert.AreEqual(42,result)
