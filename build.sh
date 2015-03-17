92d63197512606b7388b73c29e1f793eb0423287
Nathan Smith <nathan@smeef.co.uk>
2015-03-17 10:39:43 +0000

    inclusion of .owl and tidying .sh

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -e
-#npm install
+npm install
 cd ns
 
-#shopt -s nullglob
+shopt -s nullglob
 for OWL in  *.omn *.owl
 do
-	echo $OWL
 	mono ../omn2ttl/omn2ttl/bin/Release/omn2ttl.exe --uri $OWL
 done
 
