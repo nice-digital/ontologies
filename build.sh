8e56d675a1347b9be6f86567e963a4f080fe0a3c
Nathan Smith <nathan@smeef.co.uk>
2015-04-08 12:56:42 +0100

    update build.sh to play nice with submodule

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+git submodule update --remote
 cd ns
 
 for OWL in *.{omn,owl}
 do
 	echo $OWL
-	mono ../omn2ttl/omn2ttl/bin/Release/omn2ttl.exe --uri $OWL
-	java -jar ../CLLODE/CLLODE.jar $OWL
+	mono ../ld-utilities/src/omn2ttl/omn2ttl/bin/Release/omn2ttl.exe --uri $OWL
+	java -jar ../ld-utilities/src/ComLLODE/out/artifacts/CLLODE/ComLLODE.jar $OWL
 done
 
 cd -
