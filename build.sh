f67b10688813ffcb957b7a57392294e66a752ce6
Ryan Roberts <ryansroberts@gmail.com>
2015-08-11 23:14:38 +0100

    Less use of relative paths

--- build.sh
+++ build.sh
@@ -15,8 +15,8 @@ do
 	  then
 		    qs=${OWL%.*}
         qs="$qs.ttl"
-        cd $base/ld-utilities/src/csv2skos/
-        fsharpi csv2skos.fsx >> $base/ns/$qs
+        cd $base/ld-utilities
+        fsharpi ./csv2skos.fsx >> $base/ns/$qs
         cd -
 	  fi
 done
