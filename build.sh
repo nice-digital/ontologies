392181cde37f454e71bc28a36a8bc07ce9772c9e
Ryan Roberts <ryansroberts@gmail.com>
2015-09-10 19:19:00 +0100

    Build in the prefixes

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ fsharpi -I $base/ld-utilities ./csv2skos.fsx $base/ld-utilities
 for TTL in $base/ns/{,**/}*.ttl
 do
 		ld=${TTL%.*}
-    java -jar ../ld-utilities/lib/owl2jsonld/owl2jsonld.jar --all-imports --output $ld.jsonld file:$TTL
+    java -jar ../ld-utilities/lib/owl2jsonld/owl2jsonld.jar --all-imports --prefix $ld --output $ld.jsonld file:$TTL
 done
 for TTL in $base/ns/{,**/}*.ttl
 do
