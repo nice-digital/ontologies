bfd4c61f999ed7edfa67bda24e6cb1ad8d7e0070
James Kirk <james.kirk84@gmail.com>
2016-11-04 15:49:59 +0000

    Building in a docker image and cleaned up files

--- build-image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker build -t nice/ontologies .
-docker run -v $(pwd):/ontologies -it nice/ld-docker-build sh -c "cd /ontologies;./build.sh"
-cp -R ns contentlayer/
-docker build -t nice/ontologies ./contentlayer/
-rm -rf contentlayer/ns
-docker push nice/ontologies
